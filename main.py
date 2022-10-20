d1e19cda90aa21215b2e51bf976b8a2a59551191
Jose Alessandro Santos Santana <jose.santana088@academico.ifs.edu.br>
2022-10-20 18:02:00 -0300

    refatoração das funções

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-def calc_network_ip(ip, mask):
-    result_list = []
+def calc_ip_rede(ip, mask):
+    numeros_ip_rede = []
     for i in range(4):
-        result_list.append(ip[i] & mask[i])
-    return result_list
+        numeros_ip_rede.append(ip[i] & mask[i])
+    return numeros_ip_rede
 
 
 def calc_broadcast(network_ip, mask_not):
-    result_list = []
+    numeros_broadcast = []
     for i in range(4):
-        result_list.append(network_ip[i] | mask_not[i])
-    return result_list
+        numeros_broadcast.append(network_ip[i] | mask_not[i])
+    return numeros_broadcast
 
 
-def calc_not_operation(mask_split):
+def calc_operacao_not(mask_split):
     result_list = []
     for i in range(4):
         result_list.append(~mask_split[i] & 0xFF) # 0xFF (Hexadecimal) == 255 (integer) == 11111111 (binary)
@@ -31,8 +31,8 @@ def join_binary(address_split):
     return result
 
 
-def split_numbers(address):
-    return [int(valor) for valor in address.split('.')]
+def split_endereco_numeros(endereco):
+    return [int(valor) for valor in endereco.split('.')]
 
 
 def calc_first_host(network_ip_numbers):
@@ -47,16 +47,26 @@ def calc_last_host(broadcast_numbers):
     return last_host_numbers
 
 
+def count_numbers_bits_host(mask_split):
+    quantidade_bits = join_binary(mask_split).count('0')
+    return quantidade_bits
+
+
+def calc_quantidade_hosts(numero_bits_hosts):
+    quantidade_hosts = pow(2, numero_bits_hosts)
+    return quantidade_hosts
+
+
 def main():
     str_ip = input('ip: ')  # ex: 192.168.10.100
     str_mask = input('mask: ')  # ex: 255.255.255.192
 
-    ip_split = split_numbers(str_ip)
-    mask_split = split_numbers(str_mask)
+    ip_split = split_endereco_numeros(str_ip)
+    mask_split = split_endereco_numeros(str_mask)
 
-    network_ip = calc_network_ip(ip_split, mask_split)
+    network_ip = calc_ip_rede(ip_split, mask_split)
 
-    mask_not = calc_not_operation(mask_split)
+    mask_not = calc_operacao_not(mask_split)
 
     broadcast = calc_broadcast(network_ip, mask_not)
 
@@ -64,24 +74,33 @@ def main():
 
     last_host = calc_last_host(broadcast)
 
-    print(f'\n\n------ Resultado ------')
-    print(f'IP (binário): {join_binary(ip_split)}')
-    print(f'Máscara (binário): {join_binary(mask_split)}\n')
+    numero_bits_host = count_numbers_bits_host(mask_split)
 
-    print(f'IP da rede: {join_numbers(network_ip)}')
-    print(f'IP da rede (binário): {join_binary(network_ip)}\n')
+    numero_hosts = calc_quantidade_hosts(numero_bits_host)
+
+    print(f'IP (binário): {join_binary(ip_split)}')
+    print(f'Máscara (binário): {join_binary(mask_split)}')
+
+    print(f'\n\n------ Resultado ------')
+    print(f'Número De Bits De Host Por Sub-Rede: {numero_bits_host}\n')
+    
+    print(f'Número de Hosts por Sub-Rede: {numero_hosts}\n')
+
+
+    print(f'Endereço de Rede desta Sub-Rede: {join_numbers(network_ip)}')
+    print(f'Endereço de Rede desta Sub-Rede (binário): {join_binary(network_ip)}\n')
 
     print(f'Máscara (Not - binário): {join_binary(mask_not)}')
     print(f'Máscara (Not): {join_numbers(mask_not)}\n')
 
-    print(f'Primeiro Host: {join_numbers(first_host)}')
-    print(f'Primeiro Host (binário): {join_binary(first_host)}\n')
+    print(f'Endereço IPv4 do Primeiro Host nesta Sub-Rede: {join_numbers(first_host)}')
+    print(f'Endereço IPv4 do Primeiro Host nesta Sub-Rede (binário): {join_binary(first_host)}\n')
 
-    print(f'Último Host: {join_numbers(last_host)}')
-    print(f'Último Host (binário): {join_binary(last_host)}\n')
+    print(f'Endereço IPv4 do Último Host nesta Sub-Rede: {join_numbers(last_host)}')
+    print(f'Endereço IPv4 do Último Host nesta Sub-Rede (binário): {join_binary(last_host)}\n')
 
-    print(f'Broadcast: {join_numbers(broadcast)}')
-    print(f'Broadcast (binário): {join_binary(broadcast)}\n')
+    print(f'Endereço IPv4 de Broadcast nesta Sub-Rede: {join_numbers(broadcast)}')
+    print(f'Endereço IPv4 de Broadcast nesta Sub-Rede (binário): {join_binary(broadcast)}\n')
 
 
 if __name__ == '__main__':
